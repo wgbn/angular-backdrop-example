433d87c291e4c168e4867fe51e61e6b9430723cf
Walter Gandarella <walter@vivasalute.com.br>
2018-09-05 14:59:08 -0300

    inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'backdrop';
+export class AppComponent implements OnInit {
+
+    backdrop: string = 'normal';
+    height: number = 0;
+
+    @ViewChild('normal') normal: ElementRef;
+    @ViewChild('list') list: ElementRef;
+    @ViewChild('filters') filters: ElementRef;
+    @ViewChild('front') front: ElementRef;
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+        setTimeout( () => {
+            this.setHeight(this.normal.nativeElement.offsetHeight);
+        },20);
+    }
+
+    setHeight(elHeight) {
+        this.height = window.innerHeight - elHeight;
+    }
+
+    show(name) {
+        this.backdrop = name;
+        setTimeout( () => {
+            this.setHeight(this[name].nativeElement.offsetHeight);
+        },20);
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule, MatSliderModule} from "@angular/material";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatIconModule,
+        MatSliderModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
